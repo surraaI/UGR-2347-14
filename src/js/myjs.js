6afa1c2a782f29625dc2c42bd36bbc8388e7fd57
Sura Itana <141846629+surraaI@users.noreply.github.com>
2023-12-17 00:29:37 +0300

    update

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -31,7 +31,7 @@ brightModeButton.addEventListener('click', toggleBrightMode);
 document.addEventListener('DOMContentLoaded', function() {
     var aboutMeButton = document.getElementById('AboutMe');
     aboutMeButton.addEventListener('click', function() {
-        window.location.href = 'about.html'; // Replace 'about.html' with the actual path to the desired HTML page
+        window.location.href = 'about.html';
     });
 });
 
@@ -39,22 +39,20 @@ document.addEventListener('DOMContentLoaded', function() {
     var resumeButton = document.getElementById('Resume');
     resumeButton.addEventListener('click', function() {
         var link = document.createElement('a');
-        link.href = 'SuraItana.pdf'; // Replace 'SuraItna.pdf' with the actual path to the PDF file
+        link.href = 'SuraItana.pdf';
         link.download = 'SuraItana.pdf';
         link.click();
     });
 });
 
 function sendEmail(event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Get the form data
+    event.preventDefault();
     var form = document.getElementById('contactForm');
     var name = form.elements['name'].value;
     var email = form.elements['email'].value;
     var message = form.elements['message'].value;
 
-    // Send the email using EmailJS
+
     emailjs.send('service_contactSura', 'template_Suraemail', {
             name: name,
             email: email,
@@ -62,9 +60,9 @@ function sendEmail(event) {
         })
         .then(function(response) {
             console.log('Email sent successfully');
-            // Clear the form fields
+
             form.reset();
-            // Reload the page
+
             location.reload();
         }, function(error) {
             console.error('Email sending failed:', error);
