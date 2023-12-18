4485db99b2839a7d082d815477239a8f7525b4d6
Sura Itana <141846629+surraaI@users.noreply.github.com>
2023-12-18 15:09:14 +0300

    final

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -1,14 +1,3 @@
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 burgerIcon.addEventListener('click', () => { navbarMenu.classList.toggle('is-active') })
-
-const nightModeSwitch = document.getElementById('NightMode');
-const contentSection = document.getElementById('');
-
-nightModeSwitch.addEventListener('change', function() {
-    if (this.checked) {
-        contentSection.classList.add('has-background-dark', 'has-text-light');
-    } else {
-        contentSection.classList.remove('has-background-dark', 'has-text-light');
-    }
-});
